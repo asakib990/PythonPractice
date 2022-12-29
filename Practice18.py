9482fa3eff50b12af55715679512c3196267f189
SAKIB <shoriful15-11245@diu.edu.bd>
2022-12-29 23:48:44 +0600

    Upload

---
+++ Practice18.py
@@ -0,0 +1,11 @@
+a = int(input("Enter 1st Num: "))
+b = int(input("Enter 2nd Num: "))
+c = int(input("Enter 3rd Num: "))
+res = 0
+
+if a == b == c:
+    res = a + b + c
+    print(f"Final Result is: {res*3}")
+else:
+    res = a + b + c
+    print(f"Final Result is: {res}")
